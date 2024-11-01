2ecd37a466680405ab56e638c1055bf492596782
Amar2086 <omar2086pcc40sh@gmail.com>
2024-11-01 22:06:01 +0600

    first commit

---
+++ DAY 1-3/print.py
@@ -0,0 +1,20 @@
+print("Hello World")
+
+rakib=23
+zubayer=34
+don=45
+print(rakib,zubayer,don)
+my_name="omar"
+print(my_name)
+#value of the variable also can be changed 
+zubayer=56
+print(zubayer)
+a = b = c = 10
+
+print(a)
+print(b)
+print(c)
+#again change the value
+#another way to declare the value
+a,b,c=20,34.3,"zakaria"
+print(a,b,c)
